9794d07387e5d37231ac683c8f65b73ca87dfb2e
Justin Sanders <justin@pointw.com>
2016-09-01 01:19:00 -0400

    Doc Headers

--- TraitsGlobal.php
+++ TraitsGlobal.php
@@ -21,11 +21,25 @@ function class_uses_deep($class, bool $autoload = true) : array
     return array_unique($traits);
 }
 
+/**
+ * Returns whther or not a class has a trait, does not count traits that are part of the parent class.
+ * @param  string       $trait    The name of the trait you're looking for
+ * @param  mixed        $class    Class name or instance of class that we want to check
+ * @param  bool|boolean $autoload Whether function should try to autoload class
+ * @return boolean
+ */
 function has_trait(string $trait, $class, bool $autoload = true) : bool
 {
     return in_array($trait, class_uses($class, $autoload));
 }
 
+/**
+ * Returns whether or not a class has a trait, does include if a parent class has the trait
+ * @param  string       $trait    The nae of the trait you're looking for
+ * @param  mixed        $class    Class name or instance of class that we want to check
+ * @param  bool|boolean $autoload Whether function should try to autoload class
+ * @return boolean
+ */
 function has_trait_deep(string $trait, $class, bool $autoload = true) : bool
 {
     $found = false;
@@ -39,8 +53,12 @@ function has_trait_deep(string $trait, $class, bool $autoload = true) : bool
         {
             $found = true;
         }
-        $traits = array_merge($found_traits, $traits);
-    } while (!$found && $class = get_parent_class($class));
+        else
+        {
+            $traits = array_merge($found_traits, $traits);
+            $class = get_parent_class($class);
+        }
+    } while (!$found && $class);
 
     // Get traits of all parent traits
     while (!$found && !empty($traits))
@@ -50,7 +68,10 @@ function has_trait_deep(string $trait, $class, bool $autoload = true) : bool
         {
             $found = true;
         }
-        $traits = array_merge($found_traits, $traits);
+        else
+        {
+            $traits = array_merge($found_traits, $traits);
+        }
     }
 
     return $found;
