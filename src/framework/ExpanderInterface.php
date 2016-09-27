27ffda9b5ede9abc87f04f6c8c029c9feca9530f
Justin Sanders <justin@pointw.com>
2016-09-26 21:26:13 -0400

    Moved unused framework classes to the old directory

--- src/framework/ExpanderInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface IExpander {
-    public static function getStaticVariablesForClass(string $expandable_class) : array;
-    public static function setExpandableClassVariables(string $expandable_class, array $variables);
-    public static function addExpandableFunctions(array $expandable_functions);
-}
-
